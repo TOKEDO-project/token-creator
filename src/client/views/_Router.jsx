48cbb87eecbb7a728d7836dd2cdba2c0d6bbd744
Pasqualino Sorice <lino.sorice@gmail.com>
2018-08-27 15:45:14 +0200

    removed loading from AppRouter component

--- src/client/views/_Router.jsx
+++ src/client/views/_Router.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import PageNotFound from '../components/PageNotFound'
-import Loading from '../components/Loading'
-
 
 import Home from '../views/Home'
 import AddTokenAdvanced from '../views/AddTokenAdvanced'
@@ -10,7 +8,6 @@ import TokenDetails from '../views/TokenDetails'
 import HelpPage from '../views/HelpPage'
 import AboutPage from '../views/AboutPage'
 
-
 import {
   BrowserRouter as Router,
   Route,
@@ -18,26 +15,23 @@ import {
 } from 'react-router-dom'
 import './_Router.css'
 
-const AppRouter = ({web3}) => {
+const AppRouter = () => {
   return (
     <div className='main'>
       <Router>
-        {web3.loading
-          ? <Loading />
-          : <div className='pure-u-1'>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/token' >
-                <Route exact path='/add/advanced' component={AddTokenAdvanced} />
-                <Route exact path='/add/wizard' component={AddTokenWizard} />
-                <Route exact path='/:token_id' component={TokenDetails} />
-              </Route>
-              <Route exact path='/help' component={HelpPage} />
-              <Route exact path='/about' component={AboutPage} />
-              <Route render={() => <PageNotFound />} />
-            </Switch>
-          </div>
-        }
+        <div className='pure-u-1'>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/token' >
+              <Route exact path='/add/advanced' component={AddTokenAdvanced} />
+              <Route exact path='/add/wizard' component={AddTokenWizard} />
+              <Route exact path='/:token_id' component={TokenDetails} />
+            </Route>
+            <Route exact path='/help' component={HelpPage} />
+            <Route exact path='/about' component={AboutPage} />
+            <Route render={() => <PageNotFound />} />
+          </Switch>
+        </div>
       </Router>
     </div>
   )
