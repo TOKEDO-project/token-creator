ee505a7c65cf4d1bb6ec7d4fff3a9a11ed66c1ea
Pasqualino Sorice <lino.sorice@gmail.com>
2018-08-27 16:28:52 +0200

    add validation token name

--- src/client/components/steps/TokenName.jsx
+++ src/client/components/steps/TokenName.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const TokenName = ({nextFunction}) => {
-  return (
-    <div>
-      <div>Insert the name of your token:</div>
-      <input />
-      { nextFunction ? <button onClick={nextFunction} >Next</button> : null }
-    </div>
-  )
+class TokenName extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      valid: false,
+      text: ''
+    }
+  }
+
+  onChangeText = (e) => {
+    const value = e.target.value
+    const textLength = value.length
+    this.setState({
+      text: value,
+      valid: textLength > 3
+    })
+  }
+
+  render () {
+    const { nextFunction } = this.props
+    const { valid, text } = this.state
+
+    return (
+      <div>
+        <div>Insert the name of your token:</div>
+        <input value={text} onChange={this.onChangeText} />
+        {nextFunction ? <button disabled={!valid} onClick={nextFunction} >Next</button> : null}
+      </div>
+    )
+  }
 }
 
 export default TokenName
