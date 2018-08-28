027632b714f51d891b7c42084d884d412c392dcb
Pasqualino Sorice <lino.sorice@gmail.com>
2018-08-28 12:16:28 +0200

    create Atomax and Metamask componentes

---
+++ src/client/components/wallet/Atomax.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import AtomaxConnector from 'atomax-connector'
+
+class Atomax extends Component {
+  constructor (props) {
+    super(props)
+    this.state({
+      loading: true,
+      address: '',
+      txId: '',
+      data: ''
+    })
+  }
+
+  async componentDidMount () {
+    const { name, to, value } = this.props
+    const qrcode = await AtomaxConnector({
+      connectorName: name,
+      to,
+      value,
+      addressCB: address => this.setState({ address }),
+      txIdCB: tx => this.setState({ txId: tx.id })
+    })
+    this.setState({ qrcode, loading: false })
+  }
+
+  render () {
+    const { address, txId, data, loading } = this.props
+    return (
+      <div>
+        {loading
+          ? <div>Loading</div>
+          : <div>
+            { data }
+            { address && address !== '' ? <div>This is your address: {address}</div> : null }
+            { txId && txId !== '' ? <div>This is your transaction id: {txId}</div> : null }
+          </div>
+        }
+      </div>
+    )
+  }
+}
+
+export default Atomax
---
+++ src/client/components/wallet/Metamask.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { MetamaskStatus } from '../../redux/web3'
+import { connect } from 'react-redux'
+
+class Metamask extends Component {
+  render () {
+    const { web3: metamaskStatus } = this.props
+    return (
+      <button onClick={this.onClickMetamask} disabled={metamaskStatus === MetamaskStatus.LOCKED || metamaskStatus === MetamaskStatus.NOT_INSTALLED}>
+        {metamaskStatus === MetamaskStatus.LOCKED
+          ? 'Unlock Metamask'
+          : metamaskStatus === MetamaskStatus.NOT_INSTALLED
+            ? 'Install Metamask'
+            : 'Deploy the token'}
+      </button>
+    )
+  }
+}
+
+export default connect(s => s)(Metamask)
