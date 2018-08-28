59e6b5d056c57b174c7941a3b23b5fbe1723c14c
Pasqualino Sorice <lino.sorice@gmail.com>
2018-08-28 16:30:39 +0200

    added abi and bytecodes to Metamask component

--- src/client/components/wallet/Metamask.jsx
+++ src/client/components/wallet/Metamask.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 import { MetamaskStatus } from '../../redux/web3'
 import { connect } from 'react-redux'
+import simpleTokenAbi from '../../assets/abi/Token-simple'
+import startableBurnableTokenAbi from '../../assets/abi/Token-startable-burnable'
+import startableTokenAbi from '../../assets/abi/Token-startable'
+import simpleTokenBytecode from '../../assets/bytecode/Token-simple'
+import startableBurnableTokenBytecode from '../../assets/bytecode/Token-startable-burnable'
+import startableTokenBytecode from '../../assets/bytecode/Token-startable'
 
 class Metamask extends Component {
   onClickDeploy = async () => {
@@ -8,22 +14,35 @@ class Metamask extends Component {
     let abi, bytecode
     switch (type) {
       case 'startable-burnable':
+        abi = startableBurnableTokenAbi
+        bytecode = startableBurnableTokenBytecode
         break
       case 'startable':
+        abi = startableTokenAbi
+        bytecode = startableTokenBytecode
         break
       case 'simple':
+        abi = simpleTokenAbi
+        bytecode = simpleTokenBytecode
         break
     }
     const contract = new web3.eth.Contract(abi)
     // TODO: change the name of the button in waiting the transaction
-    await contract.deploy({
+    const tx = contract.deploy({
       data: bytecode,
       arguments: [name, symbol, decimals, supply]
-    }).send({
-      from: web3.address,
-      gas: 1500000,
-      gasPrice: 1500000
     })
+    const gasPrice = await web3.eth.getGasPrice()
+    let options = { from: web3.address, gasPrice, gas: 700000 }
+    const gas = await tx.estimateGas(options)
+    options.gas = gas
+    /*
+      TODO: give the possibility to aviod the nonce override:
+      const nonce = await web3.eth.getTransactionCount(wallet.address, 'pending')
+      const trx = { to: 0x0, from: web3.address, value: '0', data: abi, gas: bnUtils.times(gas, 2), nonce, gasPrice }
+      await web3.eth.sendTransaction(trx)
+    */
+    await tx.send(options)
       .on('error', error => console.log(error))
       .on('transactionHash', transactionHash => console.log(transactionHash))
       .on('receipt', receipt => console.log(receipt.contractAddress)) // contains the new contract address
