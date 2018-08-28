61dd67257c75dbd11a7f07d701700e0115edac7a
gabrielem_ <gabriele.marazzi@gmail.com>
2018-08-28 09:48:19 +0200

    add react-autocomplete-cli and bind wallet selection to redux and localstorage

--- src/client/components/steps/WalletSelection.jsx
+++ src/client/components/steps/WalletSelection.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { addToken } from '../../redux/addToken'
+import { setWalletType } from '../../redux/addToken'
+
+import './WalletSelection.css'
 
 class WalletSelection extends Component {
   constructor (props) {
@@ -10,12 +12,20 @@ class WalletSelection extends Component {
       valid: false
     }
   }
+  onClick = (e) => {
+    const { dispatch } = this.props
+    dispatch(setWalletType(e.target.getAttribute('data-wallet')))
+  }
   render () {
     const { addToken, nextFunction } = this.props
 
     return (
-      <div>
+      <div id='WalletSelection'>
         <div>Select the wallet</div>
+        <div className='pure-g'>
+          <div data-wallet='metamask' onClick={this.onClick} className={`pure-u-1-2 item${addToken.walletType === 'metamask' ? ' selected' : ''}`}><p>Metamask</p></div>
+          <div data-wallet='atomax' onClick={this.onClick} className={`pure-u-1-2 item${addToken.walletType === 'atomax' ? ' selected' : ''}`}><p>Atomax</p></div>
+        </div>
         <button onClick={nextFunction}>Create Token</button>
       </div>
     )
