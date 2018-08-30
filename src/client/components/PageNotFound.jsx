330a6bf65986f66f7b4b4232479c26c2410e8216
Adolfini Asmani <adolfinia@gmail.com>
2018-08-30 15:29:14 +0200

    started editing page not found

--- src/client/components/PageNotFound.jsx
+++ src/client/components/PageNotFound.jsx
@@ -4,9 +4,14 @@ import { withRouter } from 'react-router-dom'
 
 const PageNotFound = ({history}) => {
   return (
-    <div>
-      PageNotFound
-      <button type='button' onClick={() => history.push('/')}>Home Page</button>
+    <div id='notFound' className='d-flex flex-h-center'>
+      <div className='pure-u-22-24 pure-u-md-18-24 pure-u-sm-20-24'>
+        <div>
+           PageNotFound
+
+          <button type='button' onClick={() => history.push('/')}>Home Page</button>
+        </div>
+      </div>
     </div>
   )
 }
