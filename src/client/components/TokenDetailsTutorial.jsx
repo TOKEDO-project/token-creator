b4ad8ee29b0fef5677e746fac226be37d2003155
gabrielem_ <gabriele.marazzi@gmail.com>
2018-08-30 09:08:44 +0200

    add css for TokenDetailsTutorial

--- src/client/components/TokenDetailsTutorial.jsx
+++ src/client/components/TokenDetailsTutorial.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { translate } from 'react-i18next'
 
+import './TokenDetailsTutorial.css'
+
 const TokenDetailsTutorial = (props) => {
   const { t } = props
   return (
-    <div className=''>
-      <div>video...</div>
+    <div id='TokenDetailsTutorial'>
+      <div className='videoContent'>
+        <iframe width='560' height='315' src='https://www.youtube.com/embed/cqZhNzZoMh8' frameBorder='0' allow='autoplay; encrypted-media' allowFullScreen />
+      </div>
       <div>
         <a href='/'><button>{t('Add Token Sale')}</button></a>
       </div>
