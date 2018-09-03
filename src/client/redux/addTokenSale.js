f64675c7ff2c99c3607352cb4a264c5e2a3e373a
Pasqualino Sorice <lino.sorice@gmail.com>
2018-09-03 17:44:01 +0200

    change names of the actions of tokens sales on redux

--- src/client/redux/addTokenSale.js
+++ src/client/redux/addTokenSale.js
@@ -1,73 +1,73 @@
 import { handleActions, createAction } from 'redux-actions'
 
-export const setStep = createAction('SET_STEP',
+export const setStep = createAction('TOKEN_SALE_SET_STEP',
   (step) => {
     return step
   }
 )
 
-export const setPrice = createAction('SET_PRICE',
+export const setPrice = createAction('TOKEN_SALE_SET_PRICE',
   (price) => {
     return price
   }
 )
 
-export const setAmount = createAction('SET_AMOUNT',
+export const setAmount = createAction('TOKEN_SALE_SET_AMOUNT',
   (amount) => {
     return amount
   }
 )
 
-export const setMinContribution = createAction('SET_MIN_CONTRIBUTION',
+export const setMinContribution = createAction('TOKEN_SALE_SET_MIN_CONTRIBUTION',
   (minContribution) => {
     return minContribution
   }
 )
 
-export const setFundOwner = createAction('SET_OWNER',
+export const setFundOwner = createAction('TOKEN_SALE_SET_OWNER',
   (owner) => {
     return owner
   }
 )
 
-export const setKYC = createAction('SET_KYC',
+export const setKYC = createAction('TOKEN_SALE_SET_KYC',
   (kyc) => {
     return kyc
   }
 )
 
 export const addTokenSale = handleActions({
-  SET_STEP: (state, { payload }) => {
+  TOKEN_SALE_SET_STEP: (state, { payload }) => {
     return {
       ...state,
       step: payload
     }
   },
-  SET_PRICE: (state, { payload }) => {
+  TOKEN_SALE_SET_PRICE: (state, { payload }) => {
     return {
       ...state,
       price: payload
     }
   },
-  SET_AMOUNT: (state, { payload }) => {
+  TOKEN_SALE_SET_AMOUNT: (state, { payload }) => {
     return {
       ...state,
       amount: payload
     }
   },
-  SET_MIN_CONTRIBUTION: (state, { payload }) => {
+  TOKEN_SALE_SET_MIN_CONTRIBUTION: (state, { payload }) => {
     return {
       ...state,
       minContribution: payload
     }
   },
-  SET_OWNER: (state, { payload }) => {
+  TOKEN_SALE_SET_OWNER: (state, { payload }) => {
     return {
       ...state,
       owner: payload
     }
   },
-  SET_KYC: (state, { payload }) => {
+  TOKEN_SALE_SET_KYC: (state, { payload }) => {
     return {
       ...state,
       kyc: payload
