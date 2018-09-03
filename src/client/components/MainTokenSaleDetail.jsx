52f1fcd2434065e62cbb075be40db182a7d5e8a2
Pasqualino Sorice <lino.sorice@gmail.com>
2018-09-03 12:05:10 +0200

    add MainTokenSaleDetail logic

--- src/client/components/MainTokenSaleDetail.jsx
+++ src/client/components/MainTokenSaleDetail.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { translate } from 'react-i18next'
 
-const MainTokenSaleDetail = () => {
+const MainTokenSaleDetail = ({addMainTokenSale: { amount, state }}) => {
   return (
     <div>
-      MainTokenSaleDetail
+      <div>
+      Token Sale
+      </div>
+      <div>
+      Token for Sale
+      </div>
+      <div>
+        {state === 'authorized'
+          ? amount
+          : 'Waiting '}
+      </div>
+      <div>
+        {state === 'authorized'
+          ? <div>
+            <button>Add Token Sale</button>
+            <button>Add More Token</button>
+            <button>Remove token</button>
+          </div>
+          : null }
+      </div>
     </div>
   )
 }
 
-export default MainTokenSaleDetail
+export default translate('translations')(connect(s => s)(MainTokenSaleDetail))
