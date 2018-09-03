9cf380a678d56ded354ac924ad7b6004338f120d
Adolfini Asmani <adolfinia@gmail.com>
2018-09-03 11:25:41 +0200

    Token details menu toggle logic

--- src/client/views/TokenDetails.jsx
+++ src/client/views/TokenDetails.jsx
@@ -17,109 +17,116 @@ import MainTokenSaleAuthorize from '../components/MainTokenSaleAuthorize'
 
 import { setState } from '../redux/addMainTokenSale'
 
-const TokenDetails = (props) => {
-  const { match: { params: { tokenId } }, tokens: { transactions, receipts }, dispatch } = props
-  // Get token receipt
-  const receipt = receipts[tokenId]
-
-  if (!receipt) {
-    return (
-      <PageNotFound />
-    )
+class TokenDetails extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      showMenu: false
+    }
   }
 
-  // Get token details
-  const tokenDetails = transactions[receipt.transactionHash]
-  const { t, tokenSales, mainTokenSales, addMainTokenSale } = props
+  render () {
+    const { match: { params: { tokenId } }, tokens: { transactions, receipts }, dispatch } = this.props
+    const { showMenu } = this.state
 
-  console.log('receipt', receipt, 'tokenDetails', tokenDetails)
-  const mainTokenSale = mainTokenSales[tokenId]
-  const mainTokenSaleAddress = mainTokenSale && mainTokenSale.receipt ? mainTokenSale.receipt.contractAddress : null
-  const tokenSaleList = mainTokenSaleAddress && tokenSales[mainTokenSaleAddress] ? tokenSales[mainTokenSaleAddress] : []
+    // Get token receipt
+    const receipt = receipts[tokenId]
 
-  // Temporary logic for menu
-  let showMenu = false
+    if (!receipt) {
+      return (
+        <PageNotFound />
+      )
+    }
 
-  return (
-    <div className='pure-u-1'>
-      <div id='TokenDetails'>
-        <div className=''>
-          <div className='TokenDetailsHeader flexView flex-row flex-v-center shadow'>
-            <div className='lg-2s-24 pure-u-md-1-1 pure-u-sm-1'>
-              <a href='/'><img src={backIcon} /></a>
-            </div>
-            <div className='pure-u-lg-3-24 pure-u-md-1-4 pure-u-1-4 textCenter'>
-              <div className='d-flex flex-column flex-h-center heightBox borderRight'>
-                <h4>{t('Token Name')}:</h4>
-                <p className='breakWord'>{tokenDetails.name}</p>
-              </div>
-            </div>
-            <div className='lg-2l-24 pure-u-md-1-4 pure-u-1-4  textCenter'>
-              <div className='d-flex flex-column flex-h-center heightBox borderRight'>
-                <h4>{t('Token Symbol')}:</h4>
-                <p className='breakWord'>{tokenDetails.symbol}</p>
-              </div>
-            </div>
-            <div className='lg-2s-24 pure-u-md-1-4 pure-u-1-4  textCenter'>
-              <div className='d-flex flex-column flex-h-center heightBox borderRight'>
-                <h4>{t('Decimals')}:</h4>
-                <p className='breakWord'>{tokenDetails.decimals}</p>
-              </div>
-            </div>
-            <div className='pure-u-lg-3-24  pure-u-md-1-4 pure-u-1-4  textCenter'>
-              <div className='d-flex flex-column flex-h-center heightBox borderRight'>
-                <h4>{t('Total Supply')}:</h4>
-                <p className='breakWord'>{tokenDetails.supply}</p>
-              </div>
-            </div>
-            <div className='pure-u-lg-6-24  pure-u-md-1-2 pure-u-1-2  paddingItems'>
-              <div className='d-flex flex-column flex-h-center heightBox borderRight'>
-                <h4>{t('Token Address')}:</h4>
-                <p className='breakWord'>{tokenId}</p>
-              </div>
-            </div>
-            <div className='pure-u-lg-6-24  pure-u-md-1-2 pure-u-1-2 paddingItems'>
-              <div className='d-flex flex-column flex-h-center heightBox'>
-                <h4>{t('Token Owner')}:</h4>
-                <p className='breakWord'>{receipt.owner}</p>
-              </div>
-            </div>
-          </div>
-        </div>
+    // Get token details
+    const tokenDetails = transactions[receipt.transactionHash]
+    const { t, tokenSales, mainTokenSales, addMainTokenSale } = this.props
 
-        <div className='TokenDetailsContent'>
+    console.log('receipt', receipt, 'tokenDetails', tokenDetails)
+    const mainTokenSale = mainTokenSales[tokenId]
+    const mainTokenSaleAddress = mainTokenSale && mainTokenSale.receipt ? mainTokenSale.receipt.contractAddress : null
+    const tokenSaleList = mainTokenSaleAddress && tokenSales[mainTokenSaleAddress] ? tokenSales[mainTokenSaleAddress] : []
 
-          <div className={` ${showMenu ? 'pure-u-lg-5-24 pure-u-md-1-3 pure-u-1 pure-u-5-24'
-            : 'pure-u-lg-4-24 pure-u-md-5-24 pure-u-1 pure-u-4-24'}`}>
-            <div className={`TokenDetailsMenu ${showMenu ? 'pure-u-1' : 'widthMenuClosed'}`}>
-              <div className={` ${showMenu ? 'handleMenu' : 'closedHandleMenu'}`} onClick={() => { this.setState({ showMenu: !showMenu }) }}>
-                {showMenu ? <p >{t('Close Menu')}</p>
-                  : <p >{t('Open')}<br />{t('Menu')}</p>}
-                <i className={`fas fa-${showMenu ? 'close' : 'angle-right'}`} />
+    return (
+      <div className='pure-u-1'>
+        <div id='TokenDetails'>
+          <div className=''>
+            <div className='TokenDetailsHeader flexView flex-row flex-v-center shadow'>
+              <div className='lg-2s-24 pure-u-md-1-1 pure-u-sm-1'>
+                <a href='/'><img src={backIcon} /></a>
               </div>
-              <div className={` ${showMenu ? '' : 'hideMenuResponsive'}`}>
-                <a href='#'><button className={` ${showMenu ? 'borderBtn' : 'flex-h-center'}`} onClick={() => dispatch(setState('initialized'))}><img src={addIcon} /><p>{showMenu ? t('Add Token Sale') : null}</p></button></a>
-                <a href='/'><button className={` ${showMenu ? 'borderBtn' : 'flex-h-center'}`}><img src={padlockIcon} /><p>{showMenu ? t('Unlock The Token') : null}</p></button></a>
-                <a href='/'><button className={` ${showMenu ? 'borderBtn' : 'flex-h-center'}`}><img src={groupIcon} /><p>{showMenu ? t('Change Token Owner') : null}</p></button></a>
-                <a href='/'><button className={` ${showMenu ? 'borderBtn' : 'flex-h-center'}`}><img src={shieldIcon} /><p>{showMenu ? t('Authorize Transfer') : null}</p></button></a>
+              <div className='pure-u-lg-3-24 pure-u-md-1-4 pure-u-1-4 textCenter'>
+                <div className='d-flex flex-column flex-h-center heightBox borderRight'>
+                  <h4>{t('Token Name')}:</h4>
+                  <p className='breakWord'>{tokenDetails.name}</p>
+                </div>
+              </div>
+              <div className='lg-2l-24 pure-u-md-1-4 pure-u-1-4  textCenter'>
+                <div className='d-flex flex-column flex-h-center heightBox borderRight'>
+                  <h4>{t('Token Symbol')}:</h4>
+                  <p className='breakWord'>{tokenDetails.symbol}</p>
+                </div>
+              </div>
+              <div className='lg-2s-24 pure-u-md-1-4 pure-u-1-4  textCenter'>
+                <div className='d-flex flex-column flex-h-center heightBox borderRight'>
+                  <h4>{t('Decimals')}:</h4>
+                  <p className='breakWord'>{tokenDetails.decimals}</p>
+                </div>
+              </div>
+              <div className='pure-u-lg-3-24  pure-u-md-1-4 pure-u-1-4  textCenter'>
+                <div className='d-flex flex-column flex-h-center heightBox borderRight'>
+                  <h4>{t('Total Supply')}:</h4>
+                  <p className='breakWord'>{tokenDetails.supply}</p>
+                </div>
+              </div>
+              <div className='pure-u-lg-6-24  pure-u-md-1-2 pure-u-1-2  paddingItems'>
+                <div className='d-flex flex-column flex-h-center heightBox borderRight'>
+                  <h4>{t('Token Address')}:</h4>
+                  <p className='breakWord'>{tokenId}</p>
+                </div>
+              </div>
+              <div className='pure-u-lg-6-24  pure-u-md-1-2 pure-u-1-2 paddingItems'>
+                <div className='d-flex flex-column flex-h-center heightBox'>
+                  <h4>{t('Token Owner')}:</h4>
+                  <p className='breakWord'>{receipt.owner}</p>
+                </div>
               </div>
             </div>
           </div>
 
-          <div className='pure-u-lg-19-24 pure-u-md-2-3 pure-u-1 pure-u-19-24'>
-            <div className='TokenDetailsBody d-flex flex-v-center flex-h-center pure-u-1'>
-              {addMainTokenSale.state === 'uninitialized' ? <TokenDetailsTutorial /> : null}
-              {addMainTokenSale.state === 'initialized' ? <MainTokenSaleInit tokenId={tokenId} /> : null}
-              {addMainTokenSale.state === 'deployed' ? <MainTokenSaleAddToken tokenId={tokenId} /> : null}
-              {addMainTokenSale.state === 'token-transferred' ? <MainTokenSaleAuthorize tokenId={tokenId} /> : null}
-              {addMainTokenSale.state === 'authorized' ? <div><MainTokenSaleDetail />{tokenSaleList.length === 0 ? <TokenDetailsTutorial /> : <TokenSaleListForToken contractAddress={tokenId} />}</div> : null}
-            </div>
-          </div>
+          <div className='TokenDetailsContent'>
 
+            <div className={` ${showMenu ? 'pure-u-lg-5-24 pure-u-md-1-3 pure-u-1 pure-u-5-24'
+              : 'pure-u-lg-4-24 pure-u-md-5-24 pure-u-1 pure-u-4-24'}`}>
+              <div className={`TokenDetailsMenu ${showMenu ? 'pure-u-1' : 'widthMenuClosed'}`}>
+                <div className={` ${showMenu ? 'handleMenu' : 'closedHandleMenu'}`} onClick={() => { this.setState({ showMenu: !showMenu }) }}>
+                  {showMenu ? <p >{t('Close Menu')}</p>
+                    : <p >{t('Open')}<br />{t('Menu')}</p>}
+                  <i className={`fas fa-${showMenu ? 'close' : 'angle-right'}`} />
+                </div>
+                <div className={` ${showMenu ? '' : 'hideMenuResponsive'}`}>
+                  <a href='#'><button className={` ${showMenu ? 'borderBtn' : 'flex-h-center'}`} onClick={() => dispatch(setState('initialized'))}><img src={addIcon} /><p>{showMenu ? t('Add Token Sale') : null}</p></button></a>
+                  <a href='/'><button className={` ${showMenu ? 'borderBtn' : 'flex-h-center'}`}><img src={padlockIcon} /><p>{showMenu ? t('Unlock The Token') : null}</p></button></a>
+                  <a href='/'><button className={` ${showMenu ? 'borderBtn' : 'flex-h-center'}`}><img src={groupIcon} /><p>{showMenu ? t('Change Token Owner') : null}</p></button></a>
+                  <a href='/'><button className={` ${showMenu ? 'borderBtn' : 'flex-h-center'}`}><img src={shieldIcon} /><p>{showMenu ? t('Authorize Transfer') : null}</p></button></a>
+                </div>
+              </div>
+            </div>
+
+            <div className='pure-u-lg-19-24 pure-u-md-2-3 pure-u-1 pure-u-19-24'>
+              <div className='TokenDetailsBody d-flex flex-v-center flex-h-center pure-u-1'>
+                {addMainTokenSale.state === 'uninitialized' ? <TokenDetailsTutorial /> : null}
+                {addMainTokenSale.state === 'initialized' ? <MainTokenSaleInit tokenId={tokenId} /> : null}
+                {addMainTokenSale.state === 'deployed' ? <MainTokenSaleAddToken tokenId={tokenId} /> : null}
+                {addMainTokenSale.state === 'token-transferred' ? <MainTokenSaleAuthorize tokenId={tokenId} /> : null}
+                {addMainTokenSale.state === 'authorized' ? <div><MainTokenSaleDetail />{tokenSaleList.length === 0 ? <TokenDetailsTutorial /> : <TokenSaleListForToken contractAddress={tokenId} />}</div> : null}
+              </div>
+            </div>
+
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
-
 export default translate('translations')(connect(s => s)(TokenDetails))
