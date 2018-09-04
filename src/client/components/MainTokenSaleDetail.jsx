839146cd492e69430b07619a729d0159d43eb5e5
gabrielem_ <gabriele.marazzi@gmail.com>
2018-09-04 08:56:28 +0200

    add addTokenSale to btn

--- src/client/components/MainTokenSaleDetail.jsx
+++ src/client/components/MainTokenSaleDetail.jsx
@@ -41,7 +41,7 @@ class MainTokenSaleDetail extends React.Component {
         {(mainTokenSale.receipt && mainTokenSale.transferReceipt && mainTokenSale.setAuthorizedReceipt)
           ? <div className='btnGroup d-flex flex-v-center flex-h-between pure-u-lg-14-24 pure-u-md-1 pure-u-sm-1 pure-u-1'>
             <div className='d-flex flex-h-center pure-u-lg-1-3 pure-u-md-1-3 pure-u-sm-1 pure-u-1'>
-              <button className='pure-u-23-24'>
+              <button className='pure-u-23-24' onClick={this.addTokenSale}>
                 <span className='d-flex flex-h-center flex-v-center'>
                   <i className='fa fa-plus flex-row d-flex flex-h-center flex-v-center' />
                   {t('Add Token Sale')}
