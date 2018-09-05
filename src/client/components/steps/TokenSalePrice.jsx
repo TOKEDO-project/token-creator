22b8ebb2b210c2bfc6ca9c028d43fa99d0ef7ee7
Dario Sechi <dario.sechi.7900@gmail.com>
2018-09-05 17:26:59 +0200

    Fix missing classes

--- src/client/components/steps/TokenSalePrice.jsx
+++ src/client/components/steps/TokenSalePrice.jsx
@@ -45,7 +45,7 @@ class TokenSalePrice extends Component {
     const price = addTokenSale[tokenId].price
 
     return (
-      <div className='step pure-u-1 d-flex flex-column flex-h-between'>
+      <div className={`step ${nextFunction ? 'alone' : ''} pure-u-1 d-flex flex-column flex-h-between`}>
         <div className='top d-flex flex-row flex-h-start flex-v-center'>
           <div className='left'>
             <img className='icon' src={icon} alt='Icon' />
