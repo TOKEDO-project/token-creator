095728117b2a4505a38c61946e7928a47a5f8fd0
gabrielem_ <gabriele.marazzi@gmail.com>
2018-09-06 16:46:24 +0200

    Add transfer token

--- src/client/components/modals/TransferTokens.jsx
+++ src/client/components/modals/TransferTokens.jsx
@@ -13,7 +13,10 @@ class TransferTokens extends React.Component {
     super(props)
 
     this.state = {
-      visible: true
+      address: null,
+      amount: null,
+      visible: true,
+      transaction: null
     }
   }
   toggleVisibility = () => {
@@ -21,16 +24,68 @@ class TransferTokens extends React.Component {
     // this.setState({ visible: !this.state.visible })
     history.push(`/token/details/${tokenId}`)
   }
+  prepareTransaction = async (address, amount) => {
+    const { web3, tokens, tokenId, mainTokenSales } = this.props
+    const transactionHash = tokens.receipts[tokenId].transactionHash
+    const tokenType = tokens.transactions[transactionHash].type
+    const mainTokenSaleAddress = mainTokenSales[tokenId].receipt.contractAddress
+    console.log('---------prepareTransaction', tokenType, tokenId, mainTokenSaleAddress, amount)
+    // const transaction = await prepareTransferTokenTransaction({web3, tokenType, tokenAddress: tokenId, mainTokenSaleAddress, tokenAmount: amount})
+    this.setState({
+      address: address,
+      amount: amount,
+      transaction: transaction,
+      loading: false
+    })
+  }
+
+  onTransactionHash = (transactionHash) => {
+    /* const { dispatch, web3, tokenId } = this.props
+    console.log('OTH:', web3.address, tokenId)
+    dispatch(setState({ state: 'token-transferred', tokenAddress: tokenId }))
+    dispatch(saveTransaction(tokenId, transactionHash, { userAddress: web3.address, tokenAddress: tokenId })) */
+  }
+
+  onReceipt = (receipt) => {
+    /* const { dispatch, tokenId, addMainTokenSale: { amount } } = this.props
+    dispatch(saveTransferReceipt(tokenId, receipt, amount)) */
+  }
+  changeAmount = (e) => {
+    e.preventDefault()
+    this.setState({
+      transaction: null
+    })
+  }
   render () {
-    const { t } = this.props
-    const { visible } = this.state
+    const { t, tokenId } = this.props
+    const { visible, transaction, address, amount } = this.state
     return (
       <Modal icon={transferToken} visible={visible} title={t('Transfer Tokens')} toggleVisibility={this.toggleVisibility}>
-        <EthereumAddress />
+        {transaction
+          ? <WalletSelection connectorName='transferToken' transaction={transaction} onTransactionHash={this.onTransactionHash} onReceipt={this.onReceipt}>
+            <div className='top d-flex flex-row flex-h-start flex-v-center'>
+              <div className='left'>
+                <i className='far fa-question-circle' style={{ fontSize: '50px', color: 'grey' }} />
+              </div>
+              <div className='right d-flex flex-column flex-h-center'>
+                <span className='title'>{t(`Transfer Token`)}:</span>
+                <span className='description font-size-tiny'>
+                  {t(`You need to make the transaction to transfer the tokens.`)}
+                </span>
+                <p>
+                  {t('You are sending')}: {amount} {t('to')}: {address}  <button onClick={this.changeAmount}><i className='fas fa-undo-alt' /> Change the data</button>
+                </p>
+              </div>
+            </div>
+          </WalletSelection>
+          : <div>
+            <EthereumAddress onIsValidCB={this.setValidInput} tokenId={tokenId} />
+            <div className='separator-twentyfive' />
+            <TokenSaleTransferAmount onIsValidCB={this.setValidInput} tokenId={tokenId} />
+          </div>
+        }
         <div className='separator-twentyfive' />
-        <TokenSaleTransferAmount />
-        <div className='separator-twentyfive' />
-        <WalletSelection />
+
       </Modal>
     )
   }
--- src/client/components/steps/EthereumAddress.jsx
+++ src/client/components/steps/EthereumAddress.jsx
@@ -10,22 +10,33 @@ class EthereumAddress extends Component {
     super(props)
 
     this.state = {
-      valid: false
+      valid: false,
+      address: null
     }
+  }
+  onClickNext = (e) => {
+    e.preventDefault()
+    console.log('EthereumAddress', 'onClickNext')
+    const {onIsValidCB} = this.props
+    const {address} = this.state
+    const value = address
+    const isValid = this.validate(value)
+    console.log('address', address)
+    this.setState({ valid: isValid })
+    if (onIsValidCB && isValid) { onIsValidCB(value) }
   }
   onChangeText = (e) => {
     const value = e.target.value
-    /* Needs a real dispatch condition
-        const { dispatch } = this.props
-        dispatch(setName(value)) */
+    const isValid = this.validate(value)
     this.setState({
-      valid: this.validate(value)
+      address: value,
+      valid: isValid
     })
   }
 
   validate = (input) => {
-    const { setValid } = this.props
-    const valid = true // Needs a real validation condition
+    const { setValid, web3 } = this.props
+    const valid = web3.utils.isAddress(input)
 
     if (setValid) {
       setValid(valid)
@@ -37,29 +48,35 @@ class EthereumAddress extends Component {
     const { addToken } = this.props
     this.setState({ valid: this.validate(addToken.name) }) */
   }
-  render () {
-    const { addToken, nextFunction, t } = this.props
+  onSubmit = (e) => {
+    e.preventDefault()
     const { valid } = this.state
+    if (valid) {
+      this.onClickNext()
+    }
+  }
+  render () {
+    const { t, title = t(`Insert your ethereum address`), description = t(`Insert your ethereum address`) } = this.props
 
+    const { valid, address } = this.state
+    const errorMessage = t('Must be a valid Ethereum address')
     return (
-      <div className={`step ${nextFunction ? 'alone' : ''} pure-u-1 d-flex flex-column flex-h-between`}>
+      <div className='step pure-u-1 d-flex flex-column flex-h-between'>
         <div className='top d-flex flex-row flex-h-start flex-v-center'>
           <div className='left'>
             <img className='icon' src={icon} alt='Icon' />
           </div>
           <div className='right d-flex flex-column flex-h-center'>
-            <span className='title'>{t(`Insert your ethereum address`)}:</span>
-            <span className='description font-size-tiny'>{t(`Insert the new address you want to be owned of the token`)}</span>
+            <span className='title'>{title}:</span>
+            <span className='description font-size-tiny'>{description}</span>
           </div>
         </div>
         <form className='bottom d-flex flex-row flex-h-between'>
-          <div className={`input-box ${nextFunction ? 'pure-u-16-24' : 'pure-u-1'} d-flex flex-column flex-v-center`}>
-            <input placeholder={t(`Insert ethereum address`)} className='token-name text shadow pure-u-1' value={addToken.name} onChange={this.onChangeText} />
-            {!valid && addToken.name.length > 0 ? <div className='tooltip font-size-tiny pure-u-1 d-flex flex-row flex-v-center'><div className='triangle' />{t(`The name must be longer than 3 characters`)}</div> : null}
+          <div className='input-box pure-u-1 d-flex flex-column flex-v-center'>
+            <input placeholder={t(`Insert ethereum address`)} className='token-name text shadow pure-u-1' value={address} onChange={this.onChangeText} />
+            {!valid ? <div className='tooltip font-size-tiny pure-u-1 d-flex flex-row flex-v-center'><div className='triangle' />{errorMessage}</div> : null}
           </div>
-          {nextFunction ? <button className='next shadow pure-u-7-24' disabled={!valid} onClick={nextFunction} >
-            Next
-          </button> : null}
+          {valid ? <button className='next shadow pure-u-7-24' onClick={this.onClickNext}>{t('Next')}</button> : null }
         </form>
       </div>
     )
