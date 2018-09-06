9c92b7d1776012c2293906d5b4c64cfeb17b75a1
Adolfini Asmani <adolfinia@gmail.com>
2018-09-06 13:19:18 +0200

    Modal fixed responsive view for small devices

--- src/client/components/Modal.jsx
+++ src/client/components/Modal.jsx
@@ -8,14 +8,14 @@ export class Modal extends Component {
       <div className={`modal ${visible ? 'visible' : ''} pure-u-1`}>
         <div className='background pure-u-1' />
         <div className='container pure-u-1 d-flex flex-row flex-h-center flex-v-center'>
-          <div className='content pure-u-4-5 pure-u-sm-3-5 d-flex flex-column flex-v-center'>
+          <div className='content pure-u-lg-4-5 pure-u-xl-3-5 pure-u-1 d-flex flex-column flex-v-center'>
             <div className='header pure-u-1 d-flex flex-row flex-h-between'>
               <div className='left d-flex flex-row flex-v-center'>
                 <img className='icon' src={icon} alt='Icon' />
                 <span className='title font-weight-medium font-size-huge'>{title}</span>
               </div>
               <button onClick={toggleVisibility} type='button' className='right d-flex flex-row flex-v-center'>
-                <span className='text'>Close</span>
+                <span className='text hideTxt'>Close</span>
                 <span className='fa fa-times' />
               </button>
             </div>
