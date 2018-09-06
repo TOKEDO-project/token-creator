51a2a0bda9f2073ce9f31298ed5c4572fb10c49c
Pasqualino Sorice <lino.sorice@gmail.com>
2018-09-06 09:49:46 +0200

    add validation to token owner address

--- src/client/components/steps/TokenSaleFundOwner.jsx
+++ src/client/components/steps/TokenSaleFundOwner.jsx
@@ -16,9 +16,9 @@ class TokenSaleFundOwner extends Component {
   }
 
   onChangeText = (e) => {
-    const value = e.target.value
-
     const { dispatch, tokenId } = this.props
+
+    const value = e.target.value.trim()
     dispatch(setFundOwner({tokenAddress: tokenId, owner: value}))
     this.setState({
       valid: this.validate(value)
@@ -26,8 +26,8 @@ class TokenSaleFundOwner extends Component {
   }
 
   validate = (input) => {
-    const { setValid } = this.props
-    const valid = input.length > 1
+    const { setValid, web3 } = this.props
+    const valid = web3.utils.isAddress(input)
 
     if (setValid) {
       setValid(valid)
@@ -59,7 +59,7 @@ class TokenSaleFundOwner extends Component {
         <form className='bottom d-flex flex-row flex-h-between'>
           <div className={`input-box ${nextFunction ? 'pure-u-16-24' : 'pure-u-1'} d-flex flex-column flex-v-center`}>
             <input placeholder={t(`Insert the fund owner`)} className='token-name text shadow pure-u-1' value={owner} onChange={this.onChangeText} />
-            {!valid && owner.length > 0 ? <div className='tooltip font-size-tiny pure-u-1 d-flex flex-row flex-v-center'><div className='triangle' />{t(`The name must be longer than 3 characters`)}</div> : null}
+            {!valid && owner.length > 0 ? <div className='tooltip font-size-tiny pure-u-1 d-flex flex-row flex-v-center'><div className='triangle' />{t(`The address must be valid`)}</div> : null}
           </div>
           {nextFunction ? <button className='next shadow pure-u-7-24' disabled={!valid} onClick={nextFunction} >
             {t('Next')}
