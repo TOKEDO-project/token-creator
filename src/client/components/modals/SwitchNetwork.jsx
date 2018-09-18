67d2dc7571e9418e38bf684c6dd864d7d8fe36c9
Adolfini Asmani <adolfinia@gmail.com>
2018-09-18 14:18:15 +0200

    Css for network modal

--- src/client/components/modals/SwitchNetwork.jsx
+++ src/client/components/modals/SwitchNetwork.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
 import Modal from '../Modal'
 import { translate } from 'react-i18next'
-import icon from '../../assets/images/back.svg'
+import metamask from '../../assets/images/metamask.svg'
+import './SwitchNetwork.css'
 
 const SwitchNetwork = ({ t }) => {
   return (
-    <Modal icon={icon} title={t('Change network')} visible>
+    <Modal icon={metamask} title={t('Change network')} visible>
       {process.env.NET === 'ropsten'
-        ? <div className='pure-u-1'>
-          {t('You are on ropsten network, if you want to test this application ')}
-          <a className='link' href='https://creator-test.tokedo.io' target='_blank'>{t('Click here')}</a> {t(' or switch the network')}
+        ? <div className='pure-u-1 network'>
+          <p>
+            {t('You are on ropsten network, if you want to test this application ')}
+          </p>
+          <p>
+            <a className='link' href='https://creator-test.tokedo.io' target='_blank'>{t('Click here')}</a> {t(' or switch the network')}
+          </p>
         </div>
-        : <div className='pure-u-1'>
-          {t('You are on mainnet network, if you want to use this application in production ')}
-          <a className='link' href='https://creator.tokedo.io' target='_blank'>{t('Click here')}</a> {t(' or switch the network')}
+        : <div className='pure-u-1 network'>
+          <p>
+            {t('You are on mainnet network, if you want to use this application in production.')}
+          </p>
+          <p>
+            <a className='link' href='https://creator.tokedo.io' target='_blank'>{t('Click here')}</a> {t(' or switch the network')}
+          </p>
         </div>
       }
     </Modal>
