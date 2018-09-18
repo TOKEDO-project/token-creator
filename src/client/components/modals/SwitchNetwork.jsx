d7834038995ba5fbe6fa1c37fc59ac8e3d44b8d5
Adolfini Asmani <adolfinia@gmail.com>
2018-09-18 16:26:50 +0200

    Inserted text for modal metamask network

--- src/client/components/modals/SwitchNetwork.jsx
+++ src/client/components/modals/SwitchNetwork.jsx
@@ -6,22 +6,31 @@ import './SwitchNetwork.css'
 
 const SwitchNetwork = ({ t }) => {
   return (
-    <Modal icon={metamask} title={t('Change network')} visible>
+    <Modal icon={metamask} title={t('Oops! Something\'s wrong.')} visible>
       {process.env.NET === 'ropsten'
         ? <div className='pure-u-1 network'>
           <p>
-            {t('You are on ropsten network, if you want to test this application ')}
+            {t('Your Metamask wallet is currently operating in Ropsten Test Net.')}
           </p>
           <p>
-            <a className='link' href='https://creator-test.tokedo.io' target='_blank'>{t('Click here')}</a> {t(' or switch the network')}
+            {t('If you want to keep using the Token Creator and generate your contracts please switch to the Main Ethereum Network.')}
+          </p>
+          <p>
+            {t('If you want to run some tests instead, you can use our test environment at ')}
+            <a className='link' href='https://creator-test.tokedo.io' target='_blank'>{t('creator-test.tokedo.io')}.</a>
           </p>
         </div>
         : <div className='pure-u-1 network'>
           <p>
-            {t('You are on mainnet network, if you want to use this application in production.')}
+            {t('Your Metamask wallet is currently operating in The Main Ethereum Network. This is the Tokedo Token Creator Test Environment.')}
+          </p>
+
+          <p>
+            {t('If you want to run some tests you need to switch your wallet to the Ropsten Test Net.')}
           </p>
           <p>
-            <a className='link' href='https://creator.tokedo.io' target='_blank'>{t('Click here')}</a> {t(' or switch the network')}
+            {t('If you want to use the Token Creator to generate your contracts instead, please visit ')}
+            <a className='link' href='https://creator.tokedo.io' target='_blank'>{t('creator.tokedo.io')}.</a>
           </p>
         </div>
       }
