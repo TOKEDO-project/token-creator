012de91fcafa8218ebcf75f76d6697abca82fe06
Pasqualino Sorice <lino.sorice@gmail.com>
2018-09-21 15:09:09 +0200

    add logic to kyc page form

--- src/client/views/KYCpage.jsx
+++ src/client/views/KYCpage.jsx
@@ -2,9 +2,60 @@ import React from 'react'
 import { translate } from 'react-i18next'
 import './Pages.css'
 import '../components/modals/KycYesNo.css'
+import axios from 'axios'
+import qs from 'qs'
+
 class KYCpage extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      name: '',
+      email2: '',
+      message: '',
+      status: ''
+    }
+  }
+
+  onClickForm = async (e) => {
+    const { email2, name, message } = this.state
+    e.preventDefault()
+    if (email2 !== '' && name !== '' && message !== '') {
+      this.setState({
+        status: ''
+      })
+      try {
+        await axios.post('https://atomax.io/email/', qs.stringify({
+          email: email2,
+          name,
+          msg: message
+        }))
+        this.setState({
+          status: 'success'
+        })
+      } catch (e) {
+        this.setState({
+          status: 'error'
+        })
+      }
+    } else {
+      this.setState({
+        status: 'missing'
+      })
+    }
+  }
+
+  onChangeText = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+
+    this.setState({
+      [name]: value
+    })
+  }
+
   render () {
     const { t } = this.props
+    const { email2, name, message, status } = this.state
     return (
       <div className='kycPage step pure-u-1 d-flex flex-column flex-v-center pages'>
         <div className='maxWidth box shadow pure-u-1 pure-u-sm-1 pure-u-md-1 pure-u-lg-22-24 pure-xl-15-24'>
@@ -35,25 +86,33 @@ class KYCpage extends React.Component {
           <div className='marginTop'>
             <h3 className='textCenter'><span>{t('Leave your contact information to find out more about our KYC service.')}</span></h3>
 
-            <form className='kycForm bottom d-flex flex-row flex-h-between'>
+            <form className='kycForm bottom d-flex flex-row flex-h-between' onSubmit={this.onClickForm}>
 
               <div className='input-box pure-u-1 d-flex flex-column flex-v-center'>
                 <input type='text' name='email' hidden /> <br />
                 <div className='marginTop pure-u-1 pure-u-sm-1 pure-u-md-1-2 pure-u-lg-1-2 pure-u-xl-1-3'>
                   <label>{t('Email')}</label><br />
-                  <input className=' text shadow pure-u-1' type='text' name='email2' />
+                  <input className=' text shadow pure-u-1' type='text' name='email2' value={email2} onChange={this.onChangeText} />
                 </div>
 
                 <div className='marginTop pure-u-1 pure-u-sm-1 pure-u-md-1-2 pure-u-lg-1-2 pure-u-xl-1-3'>
                   <label>{t('Name')}</label><br />
-                  <input className=' text shadow pure-u-1' type='text' name='name' />
+                  <input className=' text shadow pure-u-1' type='text' name='name' value={name} onChange={this.onChangeText} />
                 </div>
                 <div className='marginTop pure-u-1 pure-u-sm-1 pure-u-md-1-2 pure-u-lg-1-2 pure-u-xl-1-3'>
                   <label>{t('Message')}</label><br />
-                  <input className=' text shadow pure-u-1' type='text' name='message' />
+                  <input className=' text shadow pure-u-1' type='text' name='message' value={message} onChange={this.onChangeText} />
                 </div>
                 <div className='marginTop pure-u-1 pure-u-sm-1 pure-u-md-1-2 pure-u-lg-1-2 pure-u-xl-1-3'>
-                  <button className='nextBtn shadow pure-u-1' >{t('Send Form')}</button>
+                  <button className='nextBtn shadow pure-u-1' onClick={this.onClickForm}>{t('Send Form')}</button>
+                </div>
+
+                <div className='pure-u-1 pure-u-sm-1 pure-u-md-1-2 pure-u-lg-1-2 pure-u-xl-1-3'>
+                  <p className='formMsg'>
+                    {status === 'success' ? t('Email sent') : null}
+                    {status === 'error' ? t('Error') : null}
+                    {status === 'missing' ? t('Missing parameters') : null}
+                  </p>
                 </div>
 
                 <div className='pure-u-1 pure-u-sm-1 pure-u-md-1-2 pure-u-lg-1-2 pure-u-xl-1-3'>
