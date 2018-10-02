642bfe3d4a83288ec8595d1af74b8c9f6612c759
Adolfini Asmani <adolfinia@gmail.com>
2018-10-02 11:18:06 +0200

    Github read me link

--- src/client/components/Footer.jsx
+++ src/client/components/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
   return (
     <footer className='pure-u-1 d-flex flex-h-center flex-v-center'>
-      <a href='https://github.com/TOKEDO-project/token-creator' target='_blank'><span className='fa fa-github' /></a>
+      <a href='https://github.com/TOKEDO-project/token-creator/blob/master/README.md' target='_blank'><span className='fa fa-github' /></a>
     </footer>
   )
 }
